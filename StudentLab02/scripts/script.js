9b181635878dec0f65c70f00362ee892836c3dff
cwijcarr <73864265+cwijcarr@users.noreply.github.com>
2020-12-02 21:38:55 -0700

    Add files via upload

---
+++ StudentLab02/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Jacob Carr
+    File Name: script.js
+    Date: 12/02/2020
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+    $('.hero').height($(window).height());
+    });
